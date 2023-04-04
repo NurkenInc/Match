bc049c33865c8c3543d810747bfb138720492bba
unknown <nurkenkenes7@gmail.com>
2023-04-04 14:00:25 +0600

    added theme file

---
+++ client/src/theme/index.js
@@ -0,0 +1,34 @@
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('rgb(82,75,71)', 'rgb(50,44,40)'),
+      bg: mode(
+        'linear-gradient(144deg, rgba(82,75,71,1) 0%, rgba(231,176,126,1) 61%, rgba(230,172,119,1) 100%)',
+        'linear-gradient(144deg, rgba(50,44,40,1) 0%, rgba(172,120,73,1) 61%, rgba(172,120,73,1) 100%)')
+    }
+  })
+}
+
+const components = {
+
+}
+
+const fonts = {
+  heading: ''
+}
+
+const colors = {
+
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true
+}
+
+const theme = extendTheme({ config, styles, components, fonts, colors })
+
+export default theme
