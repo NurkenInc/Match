63f7c2c0f9aa0086c4ef56fb8ab0b30f20f87bbc
unknown <nurkenkenes7@gmail.com>
2023-04-04 22:02:41 +0600

    Implemented Hero component

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import {
+  Container,
+  Box,
+  Text,
+  Heading,
+  List,
+  ListItem,
+  Stack,
+} from '@chakra-ui/react'
+
+const Hero = () => {
+  return (
+    <Container maxW={'container.xl'}>
+      <Box>
+        <Box
+          color={'whiteAlpha.700'}
+          fontSize={'26px'}
+        >
+          <Heading>Find Your Match</Heading>
+        </Box>
+        <Box>
+          <Text 
+            w={'60%'}
+            color={'yellow.500'}
+            opacity={0.7}
+            fontFamily={'Panoptica-SansBold'}
+            lineHeight={'18px'}
+          >
+            How about find you Match? Our platform it's a great resource to do it! Internships, volunteering, jobs, projects and more! Opportunities all around!
+          </Text>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default Hero
