f8c389f623db0ca4196e4cea22e2eb9ce251d379
unknown <nurkenkenes7@gmail.com>
2023-04-04 22:06:14 +0600

    Added chakra provider

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,17 +1,28 @@
-import { Provider, createStore, applyMiddleware } from 'react-redux'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
+import {
+  Home,
+  ActivitySearch,
+  ActivityDetails
+} from '../pages/index'
+
+import theme from '../theme'
 import './App.css'
 
 function App() {
   return (
-    <Provider>
-      <Router>
-        <Routes>
-          <Route path='/' />
-        </Routes>
-      </Router>
-    </Provider>
+    // <Provider>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Router>
+      </ChakraProvider>
+    // </Provider>
   )
 }
 
