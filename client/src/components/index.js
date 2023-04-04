e98b44f1a43a34e1caea434c8d498de6b5dad1a4
unknown <nurkenkenes7@gmail.com>
2023-04-04 22:05:55 +0600

    Updated imports

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from './Navbar/Navbar'
+import ActivityCard from './ActivityCard/ActivityCard'
+import Logotype from './Logotype/Logotype'
+import Hero from './Hero/Hero'
+
+export {
+  Logotype,
+  ActivityCard,
+  Navbar, 
+  Hero,
+}
