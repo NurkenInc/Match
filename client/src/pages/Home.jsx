eead9d22c0cb5dd6daa42671fcc065c7ac768bda
unknown <nurkenkenes7@gmail.com>
2023-04-05 22:35:05 +0600

    Added background image to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,34 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
 
 import { 
   Navbar,
   Hero,
 } from '../components'
 
+import { gradient01 } from '../styles'
+
 const Home = () => {
   return (
     <Box w={'100vw'} h={'100vh'}>
-      <Navbar />
-      <Hero  />   
+      <Box
+        w={'100%'}
+        h={'100%'}
+        position={'relative'}
+        overflowX={'hidden'}
+      >
+        <Image 
+          position={'absolute'}
+          src='public/university_bg.jpg'
+          opacity={0.2}
+          w={'70%'}
+          h={'80%'}
+          borderRadius={'30% 70% 100% 0% / 0% 77% 23% 100%'}
+        />
+        <Box background={gradient01}>
+          <Navbar />
+          <Hero />
+        </Box>
+      </Box>
     </Box>
   )
 }
