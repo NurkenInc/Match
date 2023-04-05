a7ec8cd740097f793c5b3e4a9efe96a76e898783
unknown <nurkenkenes7@gmail.com>
2023-04-05 21:25:34 +0600

    Added card to hero section

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -8,26 +8,42 @@ import {
   Stack,
 } from '@chakra-ui/react'
 
+import { ActivityCard } from '../index'
+
+import { testCards } from '../../contstants'
+
 const Hero = () => {
   return (
     <Container maxW={'container.xl'}>
       <Box>
-        <Box
-          color={'whiteAlpha.700'}
-          fontSize={'26px'}
-        >
-          <Heading>Find Your Match</Heading>
+        <Box>
+          <Box
+            color={'whiteAlpha.700'}
+            fontSize={'26px'}
+          >
+            <Heading>Find Your Match</Heading>
+          </Box>
+          <Box>
+            <Text 
+              w={'60%'}
+              color={'yellow.500'}
+              opacity={0.7}
+              fontFamily={'Panoptica-SansBold'}
+              lineHeight={'18px'}
+            >
+              How about find you Match? Our platform it's a great resource to do it! Internships, volunteering, jobs, projects and more! Opportunities all around!
+            </Text>
+          </Box>
         </Box>
         <Box>
-          <Text 
-            w={'60%'}
-            color={'yellow.500'}
-            opacity={0.7}
-            fontFamily={'Panoptica-SansBold'}
-            lineHeight={'18px'}
-          >
-            How about find you Match? Our platform it's a great resource to do it! Internships, volunteering, jobs, projects and more! Opportunities all around!
-          </Text>
+          {
+            testCards.map((item, index) => (
+              <ActivityCard
+                key={item.title + index}
+                {...item}
+              />
+            ))
+          }
         </Box>
       </Box>
     </Container>
