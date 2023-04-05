cc08f4e608706e4835b3778084c69b8d330b3798
unknown <nurkenkenes7@gmail.com>
2023-04-05 22:34:06 +0600

    Removed extra import

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,7 @@
 import Home from './Home'
 import ActivityDetails from './ActivityDetails'
-import ActivitySearch from './ActivitySearch'
 
 export {
   Home,
   ActivityDetails,
-  ActivitySearch
 }
