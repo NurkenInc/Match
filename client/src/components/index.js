a1a447e9a71faa51187af326e24a8c639e6304eb
unknown <nurkenkenes7@gmail.com>
2023-04-06 21:16:29 +0600

    Added ActivityList, Modals, Specifics, Tabs components to export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,20 @@ import Navbar from './Navbar/Navbar'
 import ActivityCard from './ActivityCard/ActivityCard'
 import Logotype from './Logotype/Logotype'
 import Hero from './Hero/Hero'
+import Tabs from './Tabs/Tabs'
+import Specifics from './Specifics/Specifics'
+import SearchModal from './Modal/SearchModal'
+import FilterModal from './Modal/FilterModal'
+import ActivityList from './ActivityList/ActivityList'
 
 export {
   Logotype,
   ActivityCard,
   Navbar, 
   Hero,
+  Tabs,
+  Specifics,
+  SearchModal,
+  FilterModal,
+  ActivityList,
 }
