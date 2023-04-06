52ce7c0b7177747eaa0a0f4cf283a36d0e74ad2b
unknown <nurkenkenes7@gmail.com>
2023-04-06 21:09:14 +0600

    Added Tabs compoonent

---
+++ client/src/components/Tabs/Tabs.jsx
@@ -0,0 +1,30 @@
+import {
+  Button,
+  Box
+} from '@chakra-ui/react'
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <Box>
+      {
+        tabs.map((item, index) => (
+          <Button
+            key={index}
+            onClick={() => {
+              setActiveTab(item)
+            }}
+            bg={activeTab === item ? 'purple.600' : '#303030'}
+            color={'whiteAlpha.800'}
+            fontStyle={'normal'}
+            rounded={'30px'}
+            mx={2}
+          >
+            {item}
+          </Button>
+        ))
+      }
+    </Box>
+  )
+}
+
+export default Tabs
