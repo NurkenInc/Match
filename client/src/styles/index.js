807e14609411e0182f067c24dfb3af4ae4570e45
unknown <nurkenkenes7@gmail.com>
2023-04-06 21:10:08 +0600

    Added navbar icons hover style

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,1 +1,5 @@
 export const gradient01 = `linear-gradient(97.86deg, rgba(82,75,71,1) 0%, rgba(231,176,126,1) 61%, rgba(230,172,119,1) 100%)`
+
+export const navIconsHover = {
+  color: 'teal.400'
+}
