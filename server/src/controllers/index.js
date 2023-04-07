77466b0e67f6f7d84e266be5fbd799890eafd90f
unknown <nurkenkenes7@gmail.com>
2023-04-07 23:55:15 +0600

    Imported all controllers to export

---
+++ server/src/controllers/index.js
@@ -0,0 +1,20 @@
+import createActivityCard from './ActivityCard/createActivityCard'
+import deleteActivityCard from './ActivityCard/deleteActivityCard'
+import getActivityCard from './ActivityCard/getActivityCard'
+import getActivityCards from './ActivityCard/getActivityCards'
+import likeActivityCard from './ActivityCard/likeActivityCard'
+import updateActivityCard from './ActivityCard/updateActivityCard'
+
+import signin from './User/signin'
+import signup from './User/signup'
+
+export {
+  createActivityCard,
+  deleteActivityCard,
+  getActivityCard,
+  getActivityCards,
+  likeActivityCard,
+  updateActivityCard,
+  signin,
+  signup
+}
