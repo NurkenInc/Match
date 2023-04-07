05a2225b1582b9c13e06c915a5a482f8e9523d59
unknown <nurkenkenes7@gmail.com>
2023-04-07 23:56:09 +0600

    Implemented user signin controller

--- server/src/controllers/User/signin.js
+++ server/src/controllers/User/signin.js
@@ -1,5 +1,6 @@
 import { User } from '../../models'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const signin = async (req, res) => {
   const { email, password } = req.body
@@ -7,15 +8,24 @@ const signin = async (req, res) => {
   try {
     const user = await User.findOne({ email })
 
-    if(!user) {
-      return res.status(404).json('User doesn\'t exist')
+    if (!user) {
+      return res.status(404).json({ message: 'User doesn\'t exist' })
     }
 
-    const isPasswordCorrect = 
+    const isPasswordCorrect = bcrypt.compare(user.password, password)
 
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' })
+    }
+
+    const secret = GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+    const token = jwt.sign({ email: user.email, id: user._id }, secret, { expiresIn: '3h' })
+
+    res.status(200).json({ result: user, token })
   } catch(error) {
     res.status(500).json({
-      message: error.message
+      message: 'Something went wrong'
     })
   }
 }
