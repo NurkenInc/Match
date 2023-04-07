480f58f826c3b06a66809e942b16203ca612d839
unknown <nurkenkenes7@gmail.com>
2023-04-07 23:48:35 +0600

    Added regex patterns

---
+++ client/src/constants/index.js
@@ -0,0 +1,55 @@
+import { regPassword, regGmail } from './regex'
+
+export {
+  regPassword,
+  regGmail
+}
+
+export const testCards = [
+  {
+    id: 'super-id-6',
+    image: 'https://www.applyboard.com/wp-content/uploads/2019/12/American-Universities-Feature-Image.jpg',
+    title: 'Welcome to Verdant University, where excellence and innovation intersect to provide students with a transformative educational experience. Our university is proud to offer a wide range of undergraduate and graduate programs that challenge students to grow intellectually, socially, and personally.',
+    employerName: 'Verdant University',
+    type: 'Education',
+    time: 'Full-time',
+    position: 'Student',
+    employerLogo: 'https://kentemploymentlaw.com/wp-content/uploads/2021/02/placeholder-image.jpg',
+    text: '',
+    requirements: ['fvdfvfd fd df fdvfv 12fddf fvdf dfvdsvd d df fdsdds df f', 'f', 'f'],
+    responsibilities: [],
+    benefits: [],
+    link: 'https://twitter.com/?lang=en',
+    country: 'US',
+  }
+]
+
+export const activityTypes = [
+  "Computer Science",
+  "Journalism",
+  "Media",
+  "Marketing",
+  "Education",
+  "Healthcare",
+  "Law",
+  "Engineering",
+  "Accounting",
+  "Finance",
+  "Graphic Design",
+  "Architecture",
+  "Environmental Science",
+  "Hospitality and Tourism",
+  "Performing Arts",
+  "Social Work",
+  "Psychology",
+  "Business Administration",
+  "Sports and Athletics",
+  "Fashion Design",
+  "Other",
+]
+
+export const timeTypes = [
+  "Full-time",
+  "Part-time",
+  "Other",
+]
---
+++ client/src/contstants/regex.js
@@ -0,0 +1,2 @@
+export const regPassword = /((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/
+export const regGmail = /^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/
