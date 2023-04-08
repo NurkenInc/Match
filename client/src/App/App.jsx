5792f28e3bfcfcf0ee17a5c69931297931daff16
unknown <nurkenkenes7@gmail.com>
2023-04-08 22:23:06 +0600

    Removed redux provider

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,5 +1,3 @@
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -16,18 +14,16 @@ import './App.css'
 
 function App() {
   return (
-    // <Provider>
-      <ChakraProvider theme={theme}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/:activityId' element={<ActivityDetails />} />
-            <Route path='/auth' element={<Auth />} />
-          </Routes>
-        </Router>
-      </ChakraProvider>
-    // </Provider>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/:activityId' element={<ActivityDetails />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   )
 }
 
