18229909988ef513fc7bc7ba609d22325deead11
unknown <nurkenkenes7@gmail.com>
2023-04-08 22:19:15 +0600

    Added activityCards routes

--- server/src/routes/activityCards.js
+++ server/src/routes/activityCards.js
@@ -7,18 +7,19 @@ import {
   getActivityCards,
   likeActivityCard,
   updateActivityCard
-} from '../controllers'
+} from '../controllers/index.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/', getActivityCards)
 router.get('/:id', getActivityCard)
 
-router.post('/', createActivityCard)
+router.post('/', auth, createActivityCard)
 
-router.delete('/:id', deleteActivityCard)
+router.delete('/:id', auth, deleteActivityCard)
 
-router.patch('/:id', updateActivityCard)
-router.patch('/:id/likeActivityCard', likeActivityCard)
+router.patch('/:id', auth, updateActivityCard)
+router.patch('/:id/likeActivityCard', auth, likeActivityCard)
 
 export default router
