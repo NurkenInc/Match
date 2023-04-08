16fa4c1db0f46f65edaa4ad109872378b8f78966
unknown <nurkenkenes7@gmail.com>
2023-04-08 22:18:25 +0600

    Added user routes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,10 +1,12 @@
 import express from 'express'
 
-import { signin, signup } from '../controllers'
+import { signin, signup, role } from '../controllers/index.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.post('/signin', signin)
 router.post('/signup', signup)
+router.get('/role', auth, role)
 
 export default router
