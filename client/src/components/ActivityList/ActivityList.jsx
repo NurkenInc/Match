a4e1f5e2af58bf3034649fbb2a7bb30e375f73d4
unknown <nurkenkenes7@gmail.com>
2023-04-09 22:31:16 +0600

    Added getActivityCards from server feature

--- client/src/components/ActivityList/ActivityList.jsx
+++ client/src/components/ActivityList/ActivityList.jsx
@@ -1,31 +1,56 @@
+import React, { useEffect, useState } from 'react'
 import {
   Box,
-  Container
+  Container,
+  Spinner
 } from '@chakra-ui/react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getActivityCards } from '../../actions/activityCards'
 
 import { ActivityCard } from '../index'
 
 import { testCards } from '../../constants'
 
 const ActivityList = () => {
+  const { activityCards, activityCard, isLoading, error, numberOfPages, currentPage } = useSelector((state) => state.activityCards)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getActivityCards())
+  }, [])
+
   return (
     <Container maxW={'container.xl'}>
-      <Box
-        display={'flex'}
-        flexWrap={'wrap'}
-        justifyContent={'space-between'}
-        gap={8}
-        py={8}
-      >
-        {
-          testCards.map((item, index) => (
-            <ActivityCard
-              key={item.id}
-              {...item}
-            />
-          ))
-        }
-      </Box>
+      {
+        isLoading ? 
+        <Box
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <Spinner size={'xl'} />
+        </Box> :
+        <Box
+          display={'flex'}
+          flexWrap={'wrap'}
+          justifyContent={'space-between'}
+          gap={8}
+          py={8}
+        >
+          {
+            activityCards.data !== undefined && (
+              activityCards.data.map((item, index) => (
+                <ActivityCard
+                  key={item._id}
+                  id={item._id}
+                  {...item}
+                />
+              ))
+            )
+          }
+        </Box>
+      }
     </Container>
   )
 }
