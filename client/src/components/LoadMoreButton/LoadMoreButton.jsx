a0bf9f55bfb26d2fecabffc7f1626514c221ff6c
unknown <nurkenkenes7@gmail.com>
2023-04-14 21:56:04 +0600

    added loadmorebutton compoonent

---
+++ client/src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -0,0 +1,11 @@
+import { Button, Container, Box } from '@chakra-ui/react'
+
+const LoadMoreButton = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default LoadMoreButton
