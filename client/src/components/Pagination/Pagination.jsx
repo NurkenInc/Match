2b6fa8c7a70597397688754160975fc5989e70b7
unknown <nurkenkenes7@gmail.com>
2023-04-15 15:48:02 +0600

    addition of pagination component

---
+++ client/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+import { Stack, Button, Box } from '@chakra-ui/react'
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
+import { useNavigate } from 'react-router'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { getActivityCards } from '../../actions/activityCards'
+
+const Pagination = ({ page }) => {
+  const { numberOfPages } = useSelector((state) => state.activityCards)
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  const handlePagination = (value) => {
+    if(isInvalid(value)) return
+    navigate(`/activityCards?page=${Number(page) + Number(value)}`)
+  }
+
+  const isInvalid = (value) => {
+    return Number(page) + Number(value) < 1 || Number(page) + Number(value) > numberOfPages
+  }
+
+  useEffect(() => {
+    if(page) {
+      dispatch(getActivityCards(page))
+    }
+  }, [dispatch, page])
+
+  return (
+    <Box py={5}>
+      <Stack 
+        display={'flex'} 
+        flexDir={'row'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        gap={2}
+      >
+        <Button 
+          h={6} 
+          mt={2}
+          opacity={isInvalid(-1) ? 0.4 : 1}
+          onClick={() => {
+            handlePagination(-1)
+          }}
+        >
+          <AiOutlineArrowLeft />
+        </Button>
+        <Button h={6}>
+          {page}
+        </Button>
+        <Button 
+          h={6}
+          opacity={isInvalid(1) ? 0.4 : 1}
+          onClick={() => {
+            handlePagination(1)
+          }}
+        >
+          <AiOutlineArrowRight />
+        </Button>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Pagination
