6af1e134dec38bd3126d45f6fa4b50438612c1a0
unknown <nurkenkenes7@gmail.com>
2023-04-15 15:54:37 +0600

    changed route to home page

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import {
@@ -18,8 +18,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/:activityId' element={<ActivityDetails />} />
+          <Route path='/' element={<Navigate to='/activityCards' />} />
+          <Route path='/activityCards' element={<Home />} />
+          <Route path='/activity-details/:activityId' element={<ActivityDetails />} />
           <Route path='/auth' element={<Auth />} />
         </Routes>
       </Router>
