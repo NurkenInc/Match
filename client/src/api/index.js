b56d0951a5b0676d029fca09b4c4c3d897af5053
unknown <nurkenkenes7@gmail.com>
2023-04-16 23:09:51 +0600

    Added clerc jwt to headers

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://match-bs3r.onrender.com' })
+// const API = axios.create({ baseURL: 'https://match-bs3r.onrender.com' })
+const API = axios.create({ baseURL: 'http://localhost:5000' })
 
 API.interceptors.request.use((req) => {
-  if(localStorage.getItem('profile')) {
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+  if(localStorage.getItem('clerk-db-jwt')) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem('clerk-db-jwt')}`
   }
 
   return req
@@ -15,7 +16,7 @@ export const fetchActivityCards = (page) => API.get(`/activityCards?page=${page}
 export const createActivityCard = (card) => API.post(`/activityCards`, card)
 export const updateActivityCard = (id, card) => API.patch(`/activityCards/${id}`, card)
 export const deleteActivityCard = (id) => API.delete(`/activityCards/${id}`)
-export const likeActivityCard = (id, userId) => API.patch(`/activityCards/${id}/likeActivityCard`, userId)
+export const likeActivityCard = (id, userId) => API.patch(`/activityCards/${id}/likeActivityCard`, userId) 
 
 export const signIn = (formData) => API.post('/users/signin', formData)
 export const signUp = (formData) => API.post('/users/signup', formData)
