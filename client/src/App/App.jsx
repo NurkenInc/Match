1814380593dfb0d7a66f4b351c12970d6b5d85d8
unknown <nurkenkenes7@gmail.com>
2023-04-16 23:10:14 +0600

    Added clerc provider with routes

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,5 +1,6 @@
-import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom'
+import { Route, Routes, BrowserRouter as Router, Navigate, useNavigate } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ClerkProvider, SignIn, SignUp } from '@clerk/clerk-react'
 
 import {
   Home,
@@ -12,20 +13,35 @@ import { Navbar, FooterNavbar } from '../components'
 import theme from '../theme'
 import './App.css'
 
-function App() {
+const clerkKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
+
+const ClerkProviderWithRouter = () => {
+  const navigate = useNavigate()
+  
   return (
-    <ChakraProvider theme={theme}>
-      <Router>
+    <ClerkProvider publishableKey={clerkKey} navigate={(to) => navigate(to)}>
+      <ChakraProvider theme={theme}>
         <Navbar />
         <Routes>
           <Route path='/' element={<Navigate to='/activityCards' />} />
-          <Route path='/activityCards' element={<Home />} />
+          <Route path='/activityCards/*' element={<Home />} />
           <Route path='/activity-details/:activityId' element={<ActivityDetails />} />
-          <Route path='/auth' element={<Auth />} />
+          {/* <Route path='/auth' element={<Auth />} /> */}
+          <Route path='/sign-in/*' element={<Auth><SignIn routing='path' path='/sign-in' redirectUrl='/activityCards' signUpUrl='/sign-up' /></Auth>} />
+          <Route path='/sign-up/*' element={<Auth><SignUp routing='path' path='/sign-up' redirectUrl='/activityCards' signInUrl='/sign-in' /></Auth>} />
         </Routes>
         <FooterNavbar />
-      </Router>
-    </ChakraProvider>
+      </ChakraProvider>
+    </ClerkProvider>
+  )
+}
+
+function App() {
+
+  return (
+    <Router>
+      <ClerkProviderWithRouter />
+    </Router>
   )
 }
 
