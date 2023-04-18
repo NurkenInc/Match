1f83261072a587cf51b2a475772c0b8363ef0296
unknown <nurkenkenes7@gmail.com>
2023-04-18 22:13:53 +0600

    Added clerk middleware

--- server/index.js
+++ server/index.js
@@ -4,19 +4,23 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 import { activityCardRoutes, userRoutes } from './src/routes/index.js'
+import { createClerkClient } from '@clerk/clerk-sdk-node'
 
 dotenv.config()
 
 const app = express()
 const PORT = 5000
 
+const clerk = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
+
 app.use(cors({
   origin: '*'
 }))
 app.use(express.json())
 
+app.use(clerk.expressRequireAuth())
 app.use('/activityCards', activityCardRoutes)
-app.use('/users', userRoutes)
+// app.use('/users', userRoutes)
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log(`Server is running on port: ${PORT}`)
