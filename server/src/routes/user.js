ab24a5f275329710b66800515a107558c5e5455b
unknown <nurkenkenes7@gmail.com>
2023-04-20 21:46:46 +0600

    deleted unused routes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,12 +1,12 @@
 import express from 'express'
 
 import { signin, signup, role } from '../controllers/index.js'
-import auth from '../middleware/auth.js'
+// import auth from '../middleware/clerk.js'
 
 const router = express.Router()
 
 router.post('/signin', signin)
 router.post('/signup', signup)
-router.get('/role', auth, role)
+// router.get('/role', auth, role)
 
 export default router
