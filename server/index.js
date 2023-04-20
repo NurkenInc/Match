cdb20aabe9c8dac155d524fcdd70b2ec4b58171c
unknown <nurkenkenes7@gmail.com>
2023-04-20 21:40:59 +0600

    removed middleware from all routes

--- server/index.js
+++ server/index.js
@@ -3,24 +3,19 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
-import { activityCardRoutes, userRoutes } from './src/routes/index.js'
-import { createClerkClient } from '@clerk/clerk-sdk-node'
+import { activityCardRoutes } from './src/routes/index.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = 5000
 
-const clerk = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
-
 app.use(cors({
   origin: '*'
 }))
 app.use(express.json())
 
-app.use(clerk.expressWithAuth())
 app.use('/activityCards', activityCardRoutes)
-// app.use('/users', userRoutes)
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log(`Server is running on port: ${PORT}`)
